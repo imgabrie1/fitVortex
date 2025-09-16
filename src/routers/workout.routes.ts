aee66f133f17db405f55db13e581743f16530a81
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2025-09-16 19:52:22 -0300

    feat: add get all workouts from the logged user

--- src/routers/workout.routes.ts
+++ src/routers/workout.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createWorkoutController } from "../controllers/workout.controller";
+import { createWorkoutController, getAllUserWorkoutsController } from "../controllers/workout.controller";
 import ensureDataIsValidMiddleware from "../middlewares/ensureDataIsValid.middleware";
 import { workoutSchema } from "../schemas/workout.schema";
+import ensureUserIsAuthenticatedMiddleware from "../middlewares/ensureUserIsAuthenticated.middleware";
 
 const workoutRoutes: Router = Router();
 
@@ -9,6 +10,12 @@ workoutRoutes.post(
   "",
   ensureDataIsValidMiddleware(workoutSchema),
   createWorkoutController
+);
+
+workoutRoutes.get(
+  "/:id",
+  ensureUserIsAuthenticatedMiddleware,
+  getAllUserWorkoutsController
 );
 
 export default workoutRoutes;
