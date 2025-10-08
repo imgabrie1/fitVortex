af50c279d1fa21357fbe1cde11becb8a0b9be376
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2025-10-08 18:44:03 -0300

    fix: cascade delete micro cycle to items

--- src/services/microCycle/deleteMicroCycle.service.ts
+++ src/services/microCycle/deleteMicroCycle.service.ts
@@ -1,13 +1,14 @@
 import { AppDataSource } from "../../data-source";
+import { MacroCycleItem } from "../../entities/macroCycleItem.entity";
 import { MicroCycle } from "../../entities/microCycle.entity";
 import { AppError } from "../../errors";
-
 
 export const deleteMicroCycleService = async (
   microCycleID: string,
   userID: string
 ): Promise<void> => {
   const microRepo = AppDataSource.getRepository(MicroCycle);
+  const itemRepo = AppDataSource.getRepository(MacroCycleItem);
 
   const microCycle = await microRepo.findOne({
     where: { id: microCycleID },
@@ -20,6 +21,14 @@ export const deleteMicroCycleService = async (
 
   if (microCycle.user.id !== userID) {
     throw new AppError("Só pode excluir o próprio micro ciclo", 403);
+  }
+
+  const itemsToRemove = await itemRepo.find({
+    where: { microCycle: { id: microCycleID } },
+  });
+
+  if (itemsToRemove.length > 0) {
+    await itemRepo.remove(itemsToRemove);
   }
 
   await microRepo.remove(microCycle);
