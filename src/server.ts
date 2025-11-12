bf90bfd2418eb2f2dc95310baa912295bb1ba7fd
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2025-11-12 14:34:57 -0300

    feat: add keep-alive

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
 import app from "./app";
 import { AppDataSource } from "./data-source";
+import axios from "axios";
 
 const PORT: number = Number(process.env.PORT) || 3000;
-AppDataSource.initialize().then(() => {
-    console.log("Database connected!")
+const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected!");
+
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`)
-    })
-}).catch(err => {
-    console.log(err)
-})
+      console.log(`Server is running on port ${PORT}`);
+
+      if (process.env.NODE_ENV === "production") {
+        const interval = 5 * 60 * 1000
+        setInterval(async () => {
+          try {
+            await axios.get(`${SERVER_URL}/health`);
+            console.log("Keep-alive ping enviado");
+          } catch (err) {
+            console.error("Erro ao enviar keep-alive:", (err as Error).message);
+          }
+        }, interval);
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
