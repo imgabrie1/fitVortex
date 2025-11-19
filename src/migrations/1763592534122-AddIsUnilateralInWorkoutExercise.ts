8415a64814a723e012c0b681497a11bfbee990d0
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2025-11-19 20:14:36 -0300

    fix: edit migration

--- src/migrations/1763592534122-AddIsUnilateralInWorkoutExercise.ts
+++ src/migrations/1763592534122-AddIsUnilateralInWorkoutExercise.ts
@@ -1,14 +1,21 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddIsUnilateralInWorkoutExercise1763592534122 implements MigrationInterface {
-    name = 'AddIsUnilateralInWorkoutExercise1763592534122'
+export class AddIsUnilateralInWorkoutExercise1763592534122
+  implements MigrationInterface
+{
+  name = "AddIsUnilateralInWorkoutExercise1763592534122";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "workout_exercises" ALTER COLUMN "is_unilateral" SET DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "workout_exercises"
+            ADD COLUMN "is_unilateral" boolean NOT NULL DEFAULT false
+        `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "workout_exercises" ALTER COLUMN "is_unilateral" DROP DEFAULT`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            ALTER TABLE "workout_exercises"
+            DROP COLUMN "is_unilateral"
+        `);
+  }
 }
