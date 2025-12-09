6a71e8a90ed95ab6a5a59be32d153915c6e7aa07
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2025-12-08 21:03:28 -0300

    fix: functional again :)

--- src/entities/workout.entity.ts
+++ src/entities/workout.entity.ts
@@ -18,10 +18,16 @@ export class Workout {
   @Column({ type: "varchar", length: 50 })
   name: string;
 
-  @OneToMany(() => MicroCycleItem, (item: MicroCycleItem) => item.workout)
+  @OneToMany(() => MicroCycleItem, (item: MicroCycleItem) => item.workout, {
+    cascade: true,
+  })
   cycleItems: MicroCycleItem[];
 
-  @OneToMany(() => WorkoutExercise, (workoutExercise: WorkoutExercise) => workoutExercise.workout, { cascade: true, eager: true })
+  @OneToMany(
+    () => WorkoutExercise,
+    (workoutExercise: WorkoutExercise) => workoutExercise.workout,
+    { cascade: true, eager: true }
+  )
   workoutExercises: WorkoutExercise[];
 
   @OneToOne(() => WorkoutVolume, (volume: WorkoutVolume) => volume.workout, {
